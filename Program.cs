88e5320d7f71263e5c6993907c5881737b2fd898
trung6755 <trungggrr@gmail.com>
2024-02-21 21:34:33 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThucHanhArray1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tính tổng các phần tử trong array
+            int[] numb = new int[5];
+            for (int i = 0; i < numb.Length; i++)
+            {
+                Console.Write("Input number: ");
+                numb[i] = int.Parse(Console.ReadLine()) ;
+            }
+            
+
+            int sum = 0;
+            for (int j = 0;j < numb.Length;j++)
+            {
+                sum += numb[j] ;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
